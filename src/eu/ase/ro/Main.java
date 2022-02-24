7225a3d40014d9ab3b18bd78ea0df5c5123b0560
Irina1213 <constantinescuirina19@stud.ase.ro>
2022-02-24 23:47:20 +0200

    first commit

---
+++ src/eu/ase/ro/Main.java
@@ -0,0 +1,16 @@
+package eu.ase.ro;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int k=0;
+		int s=0;
+		
+		int v[] = {0,3,-2,1,0};
+		Utils u;
+		s=Utils.function(v, k);
+		System.out.println(s);
+ 
+	}
+
+}
