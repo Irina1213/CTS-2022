41fc18e9666e291dfdc234dd9f22543be348b256
Irina Constantinescu <constantinescuirina19@stud.ase.ro>
2022-03-11 22:25:19 +0200

    Add files via upload

---
+++ Laboratory#3/README.md
@@ -0,0 +1,2 @@
+# CTS-2022
+Software Quality and Testing seminars
---
+++ Laboratory#3/src/solid/s/IUser.java
@@ -0,0 +1,9 @@
+package solid.s;
+
+public interface IUser {
+
+	public boolean login(String username, String password);
+	public boolean register(String username, String email, String password);
+	public boolean SendEmail(String content);
+	public void LogError(String errorContent);
+}
