5b9345a9645b5f515112390a3d0cdeab73b8187d
Irina Constantinescu <constantinescuirina19@stud.ase.ro>
2022-04-01 20:43:04 +0300

    Add files via upload

---
+++ Laboratory#5/src/Builder/ProgMain.java
@@ -0,0 +1,13 @@
+package Builder;
+
+public class ProgMain {
+
+	public static void main(String[] args) {
+		
+		//User u1 = new User("Madalina@csie.ase.ro", "Madalina@csie.ase.ro", "Madalina", "Pana", "Bucharest");
+		
+		//User u2 = new User ("Madalina@csie.ase.ro", "Madalina@csie.ase.ro", "Madalina", "Pana", "0745678973");
+
+	}
+
+}
---
+++ Laboratory#5/src/Singleton/ProgMain.java
@@ -0,0 +1,11 @@
+package Singleton;
+
+public class ProgMain {
+
+	public static void main(String[] args) {
+		
+
+		Database d1 = Database.getInstance();
+	}
+
+}
